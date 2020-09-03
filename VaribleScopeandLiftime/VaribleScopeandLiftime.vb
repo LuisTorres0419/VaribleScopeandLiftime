d3acdf3bcb91b65612824f3f08240cf848386e2b
luis torres <tl224627@gmail.com>
2020-09-03 08:53:37 -0600

    Add project files.

---
+++ VaribleScopeandLiftime/VaribleScopeandLiftime.vb
@@ -0,0 +1,62 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Binary
+
+Module VaribleScopeandLiftime
+
+    Dim y As Integer
+
+    Sub Main()
+        'TestTwo()
+        Dim z As String = "z in main"
+
+        'TestThree(z)
+        TestFour(z)
+
+        Console.WriteLine(z)
+
+        Console.ReadLine()
+    End Sub
+
+    Sub TestThree(ByVal anystring As String)
+
+        Console.WriteLine(anystring)
+        anystring = "not in main"
+        Console.WriteLine(anystring)
+
+    End Sub
+
+    Sub TestFour(ByRef anystring As String)
+
+        Console.WriteLine(anystring)
+        anystring = "not in main"
+        Console.WriteLine(anystring)
+
+    End Sub
+
+    Sub TestOne()
+        Dim x As Integer
+        y = 7I
+        x = 42
+        Console.WriteLine("x= " & CStr(x))
+        Console.WriteLine("y= " & CStr(y))
+
+    End Sub
+
+    Sub TestTwo()
+        Dim x As Integer
+        Console.WriteLine("x= " & CStr(x))
+        Console.WriteLine("y= " & CStr(y))
+
+        x = 5I
+        y = 4I
+        Console.WriteLine("x= " & CStr(x))
+        Console.WriteLine("y= " & CStr(y))
+
+        TestOne()
+        Console.WriteLine("x= " & CStr(x))
+        Console.WriteLine("y= " & CStr(y))
+
+        Console.ReadLine()
+    End Sub
+End Module
